99f0461532e4665657215c68c5f243a0275fc58a
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-10-06 23:58:06 +0600

    feat: add solution for fs/ delete and list

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+    await fs
+        .rm(filePath, {
+            force: false,
+            recursive: true,
+        })
+        .catch((err) => console.error("FS operation failed:", err));
 };
 
 await remove();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files");
+
 const list = async () => {
-    // Write your code here 
+    console.log(
+        await fs
+            .readdir(filePath)
+            .catch((err) => console.error("FS operation failed:", err))
+    );
 };
 
 await list();
