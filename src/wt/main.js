433c3aa4589d99cd5ac93796c7eabed5a2c5f211
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-10-08 04:54:40 +0600

    feat: add solution wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import path from "node:path";
+import url from "node:url";
+import os from 'os';
+import { Worker } from 'worker_threads';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "worker.js");
+
+const createWorkerPromise = (workerData) => {
+    return new Promise((resolve, reject) => {
+        const workerServer = new Worker(path.join(filePath), { workerData });
+        workerServer.on("message", (result) =>resolve({ status: "resolved", data: result }));
+        workerServer.on("error", () => resolve({ status: "error", data: null }));
+    });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+    const currentWorkers = os.cpus().length;
+    const resultsPromise = [];
+
+    for (let i = 0; i < currentWorkers; i++) {
+        const resultOnePromise = await createWorkerPromise(10 + i);
+        resultsPromise.push(resultOnePromise);
+    }
+
+    const workerResults = await Promise.all(resultsPromise);
+    console.log(workerResults);
 };
 
 await performCalculations();
