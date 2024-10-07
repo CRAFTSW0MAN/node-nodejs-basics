1fef23af87c87b62adfe01c83afc27464113a747
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-10-08 00:05:45 +0600

    feat: add solution zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import path from "node:path";
+import fs from "node:fs";
+import url from "node:url";
+import { createGzip } from 'zlib';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "fileToCompress.txt");
+const destinationFilePath = path.join(__dirname, "files", "archive.gz");
+
 const compress = async () => {
-    // Write your code here 
+    const gZip = createGzip();
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream(destinationFilePath);
+
+    readStream.pipe(gZip).pipe(writeStream);
+    readStream.on("error", (error) => console.log(`\nFile ${path.basename(filePath)} read operation failed`));
+    writeStream.on("error", (error) => console.log(`\nFile ${path.basename(destinationFilePath)} write operation failed`));
 };
 
 await compress();
