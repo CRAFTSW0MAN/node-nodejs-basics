d0a4db12b43955b0cb4f67480193b6541b42e9bd
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-10-08 03:51:48 +0600

    feat: add solution cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import path from "node:path";
+import url from "node:url";
+import {spawn} from 'child_process';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [filePath, ...args]);
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', 'someArgument3']);
+// spawnChildProcess([true, false, '']);
+// spawnChildProcess(['1', 2, '3']);
+
