933988db33b78c5686daf836a2f111ed456bcddc
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-10-07 11:41:43 +0600

    feat: add slution fs/ copy and rename

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files");
+
 const copy = async () => {
-    // Write your code here 
+    await fs
+        .cp(filePath, `${filePath}_copy`, {
+            errorOnExist: true,
+            recursive: true,
+            force: false
+        })
+        .catch((err) => console.error("FS operation failed:", err));
 };
 
 await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import url from "node:url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "files", "wrongFilename.txt");
+const renameFilePath = path.join(__dirname, "files", "properFilename.md");
+
 const rename = async () => {
-    // Write your code here 
+    await fs
+        .rename(filePath, renameFilePath)
+        .catch((err) => console.error("FS operation failed:", err));
 };
 
 await rename();
